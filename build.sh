01316d3e0c3e70ab7acf2d80e894a531c060734d
ssanthiya08 <s.santhiya08@gmail.com>
2026-03-14 00:29:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@
 IMAGE_NAME=devops-app
 DOCKER_USER=santhiyasasi
 
+echo "Logging into DockerHub"
+
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+
 echo "Building Docker Image"
 
 docker build -t $DOCKER_USER/$IMAGE_NAME:dev .
 
-echo "Pushing to DockerHub"
+echo "Pushing Docker Image"
 
 docker push $DOCKER_USER/$IMAGE_NAME:dev
